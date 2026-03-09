96054b899e5262b16f873ca570deecfd881b417e
Sprite Day <spriteday0@gmail.com>
2026-03-10 06:05:08 +0700

    Set up reference of simple health request

--- examples/get-health.rs
+++ examples/get-health.rs
@@ -1,4 +1,7 @@
+use std::error::Error;
+
 use clap::Parser;
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -6,8 +9,33 @@ struct Args {
     url: String,
 }
 
+#[derive(Serialize)]
+struct RpcBody {
+    id: u64,
+    jsonrpc: &'static str,
+    method: &'static str,
+    params: serde_json::Value,
+}
+
+#[derive(Deserialize, Debug)]
+struct RpcResponse<T> {
+    id: u64,
+    jsonrpc: String,
+    result: T,
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    println!("{}", args.url);
+    let client = reqwest::Client::new();
+    let body = RpcBody {
+        id: 1,
+        jsonrpc: "2.0",
+        method: "getHealth",
+        params: serde_json::Value::Null,
+    };
+    let response = client.post(&args.url).json(&body).send().await?;
+    let parsed = response.json::<RpcResponse<String>>().await?;
+    println!("{parsed:#?}");
+    Ok(())
 }
